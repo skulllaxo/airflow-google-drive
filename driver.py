f6657f9fb855f251dfce214f09e2f53ad6b41093
unknown <andrebalbino98@gmail.com>
2021-04-25 11:39:35 -0300

    csv file coor

--- driver.py
+++ driver.py
@@ -5,17 +5,18 @@ import sys
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
-
+#https://dados.educacao.sp.gov.br/dataset/endere%C3%A7os-de-escolas
 class Driver():
 
     def __init__(self):
         self.gauth = GoogleAuth()
+        self.HOME = os.getenv('HOME')
         # Try to load saved client credentials
 
     
     def authenticate(self):
         self.gauth.LoadCredentialsFile("mycreds.txt")
-
+        
         if self.gauth.credentials is None:
             # Authenticate if they're not there
             self.gauth.LocalWebserverAuth()
@@ -28,13 +29,13 @@ class Driver():
         # Save the current credentials to a file
         self.gauth.SaveCredentialsFile("mycreds.txt")
 
-    def upload_file(self):
+    def upload_file(self,folder_id,file_name):
         self.authenticate()
         drive = GoogleDrive(self.gauth)
 
-        textfile = drive.CreateFile()
-        textfile.SetContentFile('eng.txt')
+        textfile = drive.CreateFile({'parents': [{'id':folder_id}]})
+        textfile.SetContentFile(file_name)
         textfile.Upload()
 
 drive = Driver()
-drive.upload_file()
+drive.upload_file(folder_id ='1wCQlfB-q1Aj6AceKz5RLBExMo9bJFutc',file_name = '11_Escolas_Coordenadas.csv'  )
